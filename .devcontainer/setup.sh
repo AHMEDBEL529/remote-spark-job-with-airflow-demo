5ec27c3a94151c714a78178b1b0841f83e42120d
AHMED BELGANI <77440761+AHMEDBEL529@users.noreply.github.com>
2023-11-20 23:49:28 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,8 @@ mkdir -p /workspaces/remote-spark-job-with-airflow-demo/spark/app
 # Start the Astronomer development environment
 astro dev start -n --wait 8m
 
-docker build -t custom-spark -f Dockerfile-spark .
-export AIRFLOW_NETWORK=$(docker network ls --filter "name=.*airflow" --format "{{.Name}}")
-sed -i "s/AIRFLOW_NETWORK/$AIRFLOW_NETWORK/g" /workspaces/remote-spark-job-with-airflow-demo/docker-compose-spark.yml
-docker-compose -f docker-compose-spark.yml up -d
+# docker build -t custom-spark -f Dockerfile-spark .
+# export AIRFLOW_NETWORK=$(docker network ls --filter "name=.*airflow" --format "{{.Name}}")
+# sed -i "s/AIRFLOW_NETWORK/$AIRFLOW_NETWORK/g" /workspaces/remote-spark-job-with-airflow-demo/docker-compose-spark.yml
+# docker-compose -f docker-compose-spark.yml up -d
 
