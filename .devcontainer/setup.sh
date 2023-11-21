7f92847bc84eb2cc2671f3344aafc8a7b3fdd2ce
AHMED BELGANI <77440761+AHMEDBEL529@users.noreply.github.com>
2023-11-21 14:26:59 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-mkdir -p /workspaces/remote-spark-job-with-airflow-demo/spark/app
+
+# Install Astronomer CLI
+curl -sSL install.astronomer.io | sudo bash -s
+
+# Create Spark app directory
+mkdir -p ./spark/app
 
 # Wait for Docker daemon to be ready
 sleep 20
@@ -7,8 +12,14 @@ sleep 20
 # Start the Astronomer development environment
 astro dev start -n --wait 8m
 
+# Build custom Spark image
 docker build -t custom-spark -f Dockerfile-spark .
-export AIRFLOW_NETWORK=$(docker network ls --filter "name=.*airflow" --format "{{.Name}}")
-sed -i "s/AIRFLOW_NETWORK/$AIRFLOW_NETWORK/g" /workspaces/remote-spark-job-with-airflow-demo/docker-compose-spark.yml
-docker-compose -f docker-compose-spark.yml up -d
 
+# Get the Airflow network name
+export AIRFLOW_NETWORK=$(docker network ls --filter "name=.*airflow" --format "{{.Name}}")
+
+# Update Docker Compose file with Airflow network
+sed -i "s/AIRFLOW_NETWORK/$AIRFLOW_NETWORK/g" ./docker-compose-spark.yml
+
+# Start Spark cluster with Docker Compose
+docker-compose -f docker-compose-spark.yml up -d
